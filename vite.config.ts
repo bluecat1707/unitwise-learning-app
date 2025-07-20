1a2e39eed073349184e023340ee54b3ceea8c843
bluecat1707 <bluecat1707v3@gmail.com>
2025-07-20 07:38:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Update this if your repo name is different
 export default defineConfig({
-  base: '/unitwise-learning-app/', // <-- replace this with your repo name
+  base: '/unitwise-learning-app/',
   plugins: [react()],
 })
